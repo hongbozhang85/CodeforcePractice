8b18143852f9fda7728dabc5503094dd07a06ec9
Hongbo <hongbozhang18@gmail.com>
2016-11-17 06:35:09 +1100

    test HelloWorld

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by hongbo on 16-11-17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
